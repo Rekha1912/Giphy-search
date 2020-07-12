1bd86694ae0a8437f117135dd2c45afd3a41c627
Rekha1912 <rekha.0019s@gmail.com>
2020-07-12 00:35:45 -0500

    added click function for nav button on mobile view

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(function(){
-       //alert("ready..");
+    
+    //mobile button
+    $("#myMenu").click(function () {
+        if ($("#myLinks").css("display") === "block") {
+            $("#myLinks").css("display", "none");
+        } else {
+            $("#myLinks").css("display", "block");
+        }
+    });
+    
+    //search
     $("#submit").click(function(){
         let userInput = $("#search").val();
-        //alert(userInput);
-
         $.ajax({
             url: 'http://api.giphy.com/v1/gifs/search?api_key=OMbgFHquYiRsGpqYqZMjETxWI0Lkij8y&q='+userInput+'&limit='+ $("#count").val()
         }).done(function(response){
@@ -13,14 +21,10 @@ $(function(){
                         // alert( index + ": " + value.url );
                         $("#grid").append('<div class="grid-item"><img src='+value.images.fixed_width_downsampled.url+' alt="gif"/></div>')
                 });
-
-                
-                
                 let giflength = response.data.length;                 
                 $("#count").val(giflength);
-
             });
-});
+    });
 });
 
 
